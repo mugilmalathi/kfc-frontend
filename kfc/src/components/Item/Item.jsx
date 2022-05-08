b140c01185be80cccee0c4c9b164525defb4609f
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-05-08 22:35:04 +0530

    final out

--- kfc/src/components/Item/Item.jsx
+++ kfc/src/components/Item/Item.jsx
@@ -1,7 +1,18 @@
 import "./Item.css"
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export const Item = ()=>{
+export const Item = (id)=>{
+
+    const [list, setList] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8080/items/${id}`)
+        .then((res)=>{
+            setList(res.data);
+        })
+    }, [])
     return(
         <div id="item">
 
@@ -10,7 +21,7 @@ export const Item = ()=>{
             </div>
 
             <div id="item-right">
-                <h1>SIDES LOVERS 8PC. CHICKEN MEAL</h1>
+               <h1>Side Lovers 8pc. Chicken Meal.</h1>
                 <p>8 pieces of our freshly prepared World Famous Chicken, 3 large sides of your choice, and 4 biscuits.</p>
                 <p>Price: $8</p>
                 <p>calories: 2580-5640</p>
